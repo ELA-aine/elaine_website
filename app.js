e62a85c4dea75b489443eacad9b6db97debfc992
ELA-aine <54710511+ELA-aine@users.noreply.github.com>
2024-05-14 03:56:51 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const images = [...document.querySelectorAll('.imgae')];
+
+//popup
+
+const popup = document.querySelectorAll(' .popup');
+const closeBtn = document.querySelectorAll('closebtn');
+const largeIamge = document.querySelectorAll('.Sherry');
